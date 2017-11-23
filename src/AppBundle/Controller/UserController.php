ad5032316425aaf90283c95b3eb4eeab4b79a58d
Hugo Montes <hugomontes927@gmail.com>
2017-11-23 10:52:35 -0400

    Se ha finalizado con la funcion editar para usuario

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -10,8 +10,11 @@ use BackendBundle\Entity\User;
 use Symfony\Component\Validator\Constraints as Assert;
 // Importar Helper
 use AppBundle\Services\Helper;
+// Importar JwtAuth
+use AppBundle\Services\JwtAuth;
 
 class UserController extends Controller{
+
   public function newAction(Request $request){
       // echo 'Hola UserContoller';
       // die();
@@ -53,15 +56,16 @@ class UserController extends Controller{
           $user->setPassword($password);
           $user->setRole($role);
           $user->setCreatedAt($createdAt);
-          // Guardar en la base de datos
+          // Entity manager para las consultas a la BD
           $em=$this->getDoctrine()->getManager();
           // Realizando consulta, tratando de obtener usuario duplicado
           // en funcion a su email.
           $isset_user=$em->getRepository('BackendBundle:User')->findBy(array(
             'email'=>$email
           ));
-          // Verificar si existe el usuario
+          // Verificar si existe un usuario duplicado
           if(count($isset_user)==0){
+            // Guardar en la base de datos
             $em->persist($user);
             $em->flush();
             $data=array(
@@ -78,6 +82,74 @@ class UserController extends Controller{
             );
           }
         }
+      }
+      return $helper->json($data);
+  }
+
+  // Editar datos del usuario logeado
+  public function editAction(Request $request){
+      // Cargar el servicio helper
+      $helper=$this->get(Helper::class);
+      // Llamar al servicio de autenticacion JwtAuth
+      $jwt_auth=$this->get(JwtAuth::class);
+      // Conserguir el token desde la request
+      $token=$request->get('authorization',null);
+      // Verificar que el token sea correcto
+      $authCheck=$jwt_auth->checkToken($token);
+      if($authCheck){
+        // Entity manager para las consultas a la BD
+        $em=$this->getDoctrine()->getManager();
+        // Obtener los datos del usuario logeado dentro del token
+        $identity=$jwt_auth->checkToken($token, true);
+        // Obtener objeto a actualizar
+        $user=$em->getRepository('BackendBundle:User')->findOneBy(array('id'=>$identity->sub));
+        // Obtener datos que llegan por post
+        $json=$request->get("json", null);
+        // Decodificar json a objecto PHP
+        $params=json_decode($json);
+        // Array por defecto a devolver
+        $data=array(
+          'status'=>'error',
+          'code'=>500,
+          'msg'=>'Usuario no actualizado!!',
+        );
+        // Verificar que existe un json
+        if($json!=null){
+          // Recuperando datos del objeto $params
+          $name=isset($params->name)?$params->name:null;
+          $surname=isset($params->surname)?$params->surname:null;
+          $email=isset($params->email)?$params->email:null;
+          $password=isset($params->password)?$params->password:null;
+          $updatedAt=new \Datetime("now");
+          // Validando email
+          $emailConstraint=new Assert\Email();
+          $emailConstraint->message='Este email no es valido';
+          $validate_email=$this->get('validator')->validate($email, $emailConstraint);
+          // Validando datos, exigiendo datos obligatorios
+          if($email!=null && count($validate_email)==0 && $password!=null && $name!=null && $surname!=null){
+            // Setear valores a actualizar
+            $user->setName($name);
+            $user->setSurname($surname);
+            $user->setEmail($email);
+            $user->setPassword($password);
+            $user->setUpdatedAt($updatedAt);
+            // Actualizar usuario en la base de datos
+            $em->persist($user);
+            $em->flush();
+            $data=array(
+                'status'=>'success',
+                'code'=>200,
+                'msg'=>'Usuario actualizado!!',
+                'user'=>$user
+            );
+          }
+        }
+      }else{
+        $data=array(
+          'status'=>'error',
+          'code'=>500,
+          'msg'=>'Autorizacion no valida!!',
+        );
       }
       return $helper->json($data);
   }
